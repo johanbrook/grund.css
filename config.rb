2b52a58840bda1a1e51d540daa7122c79cc5d810
Johan Brook <johan.jvb@gmail.com>
2012-06-17 20:41:13 +0200

    Added Compass config file

---
+++ config.rb
@@ -0,0 +1,16 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "stylesheets"
+sass_dir = "scss"
+images_dir = "images"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = (environment == :production) ? :compressed : :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
